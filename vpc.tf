e20bc246d596121b5928dc2c872926f68ff0fbd7
bobburi <bobburirajesh006@gmail.com>
2024-09-30 09:56:07 +0530

    added

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
 resource "google_compute_network" "vpc_network" {
   project                 = "test123-436512"
-  name                    = "vpc-network"
-  auto_create_subnetworks = true
+  name                    = "vpc-ork"
+  auto_create_subnetworks = false
   mtu                     = 1460
 }
 
 resource "google_compute_network" "vpc_network-1" {
   project                 = "test123-436512"
-  name                    = "vpc-network-1"
+  name                    = "vpc-11"
   auto_create_subnetworks = true
   mtu                     = 1460
 }
